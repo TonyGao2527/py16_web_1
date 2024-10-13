aa669804415cdf17401da0e48565b91da189c8e8
高修宇 <5942527@qq.com>
2024-10-13 23:02:18 +0800

    records.vue才刚开始写

--- src/views/Records.vue
+++ src/views/Records.vue
@@ -3,7 +3,25 @@
 </template>
 
 <script>
+export default {
+	data() {
+		return {
+			// 执行记录数据数组的列表
+			recordList: [],
+			// 最多900页
+			count: 900,
+			// 第 1 页
+			page: 1,
+			// 每一页10条数据
+			size: 10
+		};
+	},
+	// 页面刷新时调用的钩子
+	// 一进入页面就获取项目信息；
+	created() {
+		this.getProjectrecords();
+	},
+}
 </script>
 
-<style>
-</style>
+<style></style>
